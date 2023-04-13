996a99f9d5bd4686eb5de5eef755e59180729ab2
Lucianosolari <luciano3924@gmail.com>
2023-04-13 17:51:27 -0300

    Patrones

---
+++ Patrones-lab4/Patrones-lab4/FactoryMethod.cs
@@ -0,0 +1,64 @@
+﻿static void Main(string[] args)
+    {
+        // Crear drones repartidores
+        IRepartidorCreator droneCreator = new DroneCreator();
+        IRepartidor drone = droneCreator.CrearRepartidor();
+
+
+        drone.EntregarPedido();
+
+
+        // Crear repartidor humano
+        IRepartidorCreator repartidorHumanoCreator = new RepartidorHumanoCreator();
+        IRepartidor repartidorHumano = repartidorHumanoCreator.CrearRepartidor();
+
+        repartidorHumano.EntregarPedido();
+    }
+
+public interface IRepartidor
+{
+    void EntregarPedido();
+}
+
+
+public class Drone : IRepartidor
+{
+    public void EntregarPedido()
+    {
+        Console.WriteLine("Entregando pedido con Drone");
+    }
+}
+
+public class RepartidorHumano : IRepartidor
+{
+    public void EntregarPedido()
+    {
+        Console.WriteLine("Entregando pedido con Repartidor Humano");
+    }
+}
+
+
+public interface IRepartidorCreator
+{
+    IRepartidor CrearRepartidor();
+}
+
+
+public class DroneCreator : IRepartidorCreator
+{
+    public IRepartidor CrearRepartidor()
+    {
+        return new Drone();
+    }
+}
+
+
+public class RepartidorHumanoCreator : IRepartidorCreator
+{
+    public IRepartidor CrearRepartidor()
+    {
+        return new RepartidorHumano();
+    }
+}
+
+
---
+++ Patrones-lab4/Patrones-lab4/Observer.cs
@@ -0,0 +1,56 @@
+﻿static void Main(string[] args)
+{
+    var pedido = new Pedido();
+    var cliente = new Cliente();
+
+    pedido.AgregarObservador(cliente);
+
+    pedido.CambiarEstado("En preparación");
+    pedido.CambiarEstado("Enviado");
+    pedido.CambiarEstado("Entregado");
+}
+
+public class Pedido
+{
+    private List<IObserver> observadores = new List<IObserver>();
+
+    public string Estado { get; private set; }
+
+    public void CambiarEstado(string nuevoEstado)
+    {
+        Estado = nuevoEstado;
+        NotificarObservadores();
+    }
+
+    public void AgregarObservador(IObserver observador)
+    {
+        observadores.Add(observador);
+    }
+
+    public void EliminarObservador(IObserver observador)
+    {
+        observadores.Remove(observador);
+    }
+
+    private void NotificarObservadores()
+    {
+        foreach (var observador in observadores)
+        {
+            observador.Actualizar(this);
+        }
+    }
+}
+
+public interface IObserver
+{
+    void Actualizar(Pedido pedido);
+}
+
+public class Cliente : IObserver
+{
+    public void Actualizar(Pedido pedido)
+    {
+        Console.WriteLine($"El pedido está en estado {pedido.Estado}");
+    }
+}
+
